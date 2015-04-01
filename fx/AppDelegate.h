f3b7354831323b7929ad0ee87165590f78ec7a0d
chenso <chen.s@wustl.edu>
2015-04-01 01:47:42 -0500

    Initial Commit

---
+++ fx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fx
+//
+//  Created by Songge Chen on 4/1/15.
+//  Copyright (c) 2015 Songge Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fx
+//
+//  Created by Songge Chen on 4/1/15.
+//  Copyright (c) 2015 Songge Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
